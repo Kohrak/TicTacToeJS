7827e39c36557fc4eb72c6ae42b3659c13e751e2
Kohrak <Gabriel@PPM.local>
2018-03-19 15:22:20 +0000

    add board arr and possible winning moves arrays

--- board.js
+++ board.js
@@ -1,1 +1,14 @@
 var cells = document.querySelectorAll("td");
+var board = [];
+for (var i = 0; i < 9; i++){
+  board.push("n")
+}
+var winningMoves = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8]
+]
